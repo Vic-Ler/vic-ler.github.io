3892261375e87e2dc287358e4edf58b38b84deed
Vic-Ler <152528935+Vic-Ler@users.noreply.github.com>
2024-02-12 18:38:09 +0100

    Update script.js

--- secret/script.js
+++ secret/script.js
@@ -5,26 +5,26 @@ document.getElementById("codeForm").addEventListener("submit", function(event) {
 
     // Define an object to map codes to corresponding HTML file names
     var codeToPage = {
-        "1357": "page01.html",
-        "2468": "page02.html",
-        "1111": "page03.html",
+        "1348": "page01.html",
+        "1345": "page02.html",
+        "1305": "page03.html",
         "1410": "page04.html",
-        "1305": "page05.html",
-        "1997": "page06.html",
-        "1996": "page07.html",
-        "6666": "page08.html",
-        "0303": "page09.html",
-        "2356": "page10.html",
-        "3467": "page11.html",
-        "8479": "page12.html",
-        "4578": "page13.html",
-        "9074": "page14.html",
-        "8888": "page15.html",
-        "2094": "page16.html",
-        "1078": "page17.html",
-        "8822": "page18.html",
-        "3333": "page19.html",
-        "0006": "page20.html"
+        "1997": "page05.html",
+        "1996": "page06.html",
+        "1160": "page07.html",
+        "1140": "page08.html",
+        "2020": "page09.html",
+        "2288": "page10.html",
+        "1194": "page11.html",
+        "6118": "page12.html",
+        "8231": "page13.html",
+        "8881": "page14.html",
+        "1111": "page15.html",
+        "8118": "page16.html",
+        "2038": "page17.html",
+        "8181": "page18.html",
+        "1842": "page19.html",
+        "4058": "page20.html"
     };
 
     // Check if the code exists in the mapping
